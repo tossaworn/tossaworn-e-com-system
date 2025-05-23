e6b7958bd0bd9e5b51407eec837de244fb636699
Tossaworn Srinutchasart <tossawornsri@gmail.com>
2025-05-23 14:29:09 +0700

    up 9.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>E-com System 8.0.1 test CI/CD testdeploy</h1>
+      <h1>E-com System 9.0.0 test CI/CD testdeploy</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
